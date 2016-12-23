0c3290c024e48c2e25a042bcb37741b20a12f36f
Sam Stenvall <sam.stenvall@nordsoftware.com>
2016-12-23 14:28:12 +0200

    Add missing @throws declaration to the interface

--- src/Provider/ProviderInterface.php
+++ src/Provider/ProviderInterface.php
@@ -2,6 +2,8 @@
 
 namespace Jalle19\CertificateParser\Provider;
 
+use Jalle19\CertificateParser\Provider\Exception\ProviderException;
+
 /**
  * Interface ProviderInterface
  * @package Jalle19\CertificateParser\Provider
@@ -11,6 +13,8 @@ interface ProviderInterface
 
     /**
      * @return resource the raw X.509 certificate
+     *
+     * @throws ProviderException
      */
     public function getRawCertificate();
 
