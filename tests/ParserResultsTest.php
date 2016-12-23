526274f2df01d86a54457f00bd95d2ece66e346c
Sam Stenvall <sam.stenvall@nordsoftware.com>
2016-12-23 14:14:49 +0200

    Add more tests for ParserResult using the new LocalFileProvider

---
+++ tests/ParserResultsTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Jalle19\CertificateParser\Tests;
+
+use AcmePhp\Ssl\ParsedCertificate;
+use Jalle19\CertificateParser\Parser;
+use Jalle19\CertificateParser\ParserResults;
+use Jalle19\CertificateParser\Provider\LocalFileProvider;
+
+/**
+ * Class ParserResultsTest
+ * @package Jalle19\CertificateParser\Tests
+ */
+class ParserResultsTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var ParserResults
+     */
+    private $parserResults;
+
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $parser   = new Parser();
+        $provider = new LocalFileProvider(__DIR__ . '/../resources/ssl-cert-snakeoil.pem');
+
+        $this->parserResults = $parser->parse($provider);
+    }
+
+
+    /**
+     * Tests that getParsedCertificate() works correctly
+     */
+    public function testGetParsedCertificate()
+    {
+        $this->assertInstanceOf(ParsedCertificate::class, $this->parserResults->getParsedCertificate());
+    }
+
+
+    /**
+     * Tests that getRawCertificate() works correctly
+     */
+    public function testGetRawCertificate()
+    {
+        $this->assertCount(1, $this->parserResults->getRawCertificate()['extensions']);
+    }
+
+}
